538a72ecd00065aeb6bf97e67173c0dc43ea4a51
Blackman <DBlackman@int.co.chenango.ny.us>
2024-09-11 15:58:22 -0400

    End of day

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ const filterSelect = document.getElementById("filter-select");
 fetch("data.json")
     .then( response => response.json())
     .then( (data) => {
-       console.log(data)
-       
-       data.forEach( (country) => {
-        countryWrapper.innerHTML += 
-        `<div class = "country-card">
-            <div class = "flag">
-                <img class = "flag-img" src = "${country.flag}">
-            </div>
-            <div class = "country-info">
-            <h2>${country.name}</h2>
-                <p><b>Population:</b> ${country.population}</p>
-                <p><b>Region:</b> ${country.region}</p>
-                <p><b>Capital:</b> ${country.capital}</p>
-            </div>
-        <div>
-        `
-       })
+        const filter = document.getElementById("filter-select")
+        const regionList = []
+        console.log(data)
+
+
+       // Add country cards
+        data.forEach( (country) => {
+            countryWrapper.innerHTML += 
+            `<div class = "country-card">
+                <div class = "flag">
+                    <img class = "flag-img" src = "${country.flag}">
+                </div>
+                <div class = "country-info">
+                <h2>${country.name}</h2>
+                    <p><b>Population:</b> ${country.population}</p>
+                    <p><b>Region:</b> ${country.region}</p>
+                    <p><b>Capital:</b> ${country.capital}</p>
+                </div>
+            <div>
+            `
+            if (!regionList.includes(country.region)) {
+                regionList.push(country.region)  
+            }
+        })
+
+        // Add options to filter
+        regionList.forEach( (region) => {
+            filter.innerHTML += 
+                `<option value = "${region}">${region}</option>`
+        })
+
 
     })
 
